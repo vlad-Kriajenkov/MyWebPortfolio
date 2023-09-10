ef748ffeb777aae00578168d0d14cbad00aab5e0
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-09-11 00:15:04 +0300

    Add Page Home

---
+++ src/page/home/Home.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Conatiner } from './home.styled';
+const Home = () => {
+  return <Conatiner>home</Conatiner>;
+};
+
+export default Home;
---
+++ src/page/home/home.styled.tsx
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled'
+
+
+export const Conatiner = styled.div`
+  flex-grow: 1;
+`;
