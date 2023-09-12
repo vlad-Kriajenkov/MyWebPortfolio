3c0533f6dd62912628a5331ed95068bed797340f
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-09-12 09:41:19 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { HomeLayout } from './layout/HomeLayout/';
+import { HomeLayout, AboutLayout, AboutProfessionalLayout } from './layout';
+import { AboutPersonal, AboutHobbies, Frilance } from 'page/about';
 import Home from './page/home/Home';
+// import About from 'page/about/About';
+
 function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <Routes>
         <Route path="/" element={<HomeLayout />}>
           <Route index element={<Home />} />
+          <Route path="about" element={<AboutLayout />}>
+            <Route path="professional" element={<AboutProfessionalLayout />}>
+              <Route path="frilance" element={<Frilance />} />
+              <Route path="skills" />
+              <Route path="certificates" />
+            </Route>
+            <Route path="personal" element={<AboutPersonal />} />
+            <Route path="hobbies" element={<AboutHobbies />} />
+          </Route>
         </Route>
       </Routes>
     </div>
