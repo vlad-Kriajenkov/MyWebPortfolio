2faf485792a2b79f04b99d0122b93d4d95c3ea6d
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-09-12 09:37:03 +0300

    Update colors.js

--- src/assets/colors/colors.js
+++ src/assets/colors/colors.js
@@ -5,5 +5,14 @@ export const myColor = {
   },
   text: {
     primary: '#607b96',
+    seconder: '#E5E9F0',
+    accent: '#5565E8',
+  },
+  link: {
+    defoult: '#607b96',
+    active: '#81A1C1',
+  },
+  folder: {
+    primary: '#fea55f',
   },
 };
