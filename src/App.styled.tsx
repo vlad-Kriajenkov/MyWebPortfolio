a677cd235389e5f7d8e04f911c547111d2b9adf3
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-09-14 09:38:30 +0300

    Create App.styled.tsx

---
+++ src/App.styled.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+import { myColor } from 'assets/colors/colors';
+export const Container = styled.div`
+  background-color: ${myColor.backGraund.body};
+  width: 100vw;
+  height: 100vh;
+
+  padding: 2rem;
+`;
