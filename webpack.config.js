3d31b705347161efe0494d86ae97a7dac2a52ce9
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-09-14 09:33:42 +0300

    UpDate Config

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,22 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: path.join('img', '[name].[contenthash][ext]'),
   },
   module: {
     rules: [
       {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: "asset/img",
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset/resource',
+        generator: {
+          filename: path.join('icons', '[name].[contenthash][ext]'),
+        },
       },
     ],
   },
+  
 };
