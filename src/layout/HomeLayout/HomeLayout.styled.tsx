c54a36ad4bf4855b2b2a677253bd3b34a084dcd0
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-09-14 09:35:33 +0300

    Update HomeLayout.styled.tsx

--- src/layout/HomeLayout/HomeLayout.styled.tsx
+++ src/layout/HomeLayout/HomeLayout.styled.tsx
@@ -3,9 +3,9 @@ import { myColor } from 'assets/colors/colors';
 export const Conatainer = styled.div`
   display: flex;
   flex-direction: column;
-  
+
   height: 100%;
-  background-color: #011627;
+  background-color: ${myColor.backGraund.main};
   border-radius: 10px;
   border: 1px solid ${myColor.border.primary};
 `;
