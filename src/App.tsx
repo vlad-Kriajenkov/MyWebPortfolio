886f3adbd6fc1b767bf502d6b4f234929bac980c
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-09-15 10:57:55 +0300

    Update app
    Add new route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Container } from './App.styled';
-import { HomeLayout, AboutLayout, AboutProfessionalLayout } from './layout';
-import { AboutPersonal, AboutHobbies, Frilance, Cpaecom ,Skills} from 'page/about';
+import {
+  HomeLayout,
+  AboutLayout,
+  AboutProfessionalLayout,
+  AboutPersonalLayout,
+} from './layout';
+import { AboutHobbies, Frilance, Cpaecom, Skills } from 'page/about';
 import Home from './page/home/Home';
- 
+import { Certificates } from 'page/about/About-Professional/Certificates/Certificates';
+import AboutMe from 'page/about/About-Personal/About-me/AboutMe';
 
 function App() {
   return (
@@ -16,10 +22,12 @@ function App() {
             <Route path="professional" element={<AboutProfessionalLayout />}>
               <Route path="frilance" element={<Frilance />} />
               <Route path="cpaecom" element={<Cpaecom />} />
-              <Route path="skills"  element={<Skills/>}/>
-              <Route path="certificates" />
+              <Route path="skills" element={<Skills />} />
+              <Route path="certificates" element={<Certificates />} />
             </Route>
-            <Route path="personal" element={<AboutPersonal />} />
+            <Route path="personal" element={<AboutPersonalLayout />}>
+              <Route path="about_me" element={<AboutMe />} />
+            </Route>
             <Route path="hobbies" element={<AboutHobbies />} />
           </Route>
         </Route>
