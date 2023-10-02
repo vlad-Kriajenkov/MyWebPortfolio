e95605516e64285707a4d525d840b6156c4c9a8f
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-02 10:36:43 +0300

    Update App.styled.tsx

--- src/App.styled.tsx
+++ src/App.styled.tsx
@@ -3,7 +3,10 @@ import { myColor } from 'assets/colors/colors';
 export const Container = styled.div`
   background-color: ${myColor.backGraund.body};
   width: 100vw;
-  height: 100vh;
+  height: 100dvh;
 
   padding: 2rem;
+  @media screen and (max-width: 420px) {
+    padding: 0.4rem;
+  }
 `;
