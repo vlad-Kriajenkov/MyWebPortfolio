db4aa70ad849d38407ae5d3ecfc6db0fd49b96fb
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-02 10:35:59 +0300

    Update

--- src/page/Projects/Projects.styled.tsx
+++ src/page/Projects/Projects.styled.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { myColor } from 'assets/colors/colors';
 
 export const Container = styled.div`
   display: flex;
@@ -13,4 +14,12 @@ export const WrapperProject = styled.div`
 `;
 export const Box = styled.div`
   width: calc((100% - 30px) / 3);
+`;
+
+export const Info = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  color: ${myColor.text.seconder};
 `;
--- src/page/Projects/Projects.tsx
+++ src/page/Projects/Projects.tsx
@@ -1,14 +1,30 @@
 import { CardProject, CheckBox, PageNameLayout } from 'components';
-import { Container, WrapperProject, Box } from './Projects.styled';
+import { Container, WrapperProject, Box, Info } from './Projects.styled';
 import React, { useCallback, useEffect, useState } from 'react';
 import IconArrya from 'assets/json/icon.json';
 import ProjectArrya from 'assets/json/project.json';
+
+interface Item {
+  id: number;
+  nameProject: string;
+  about: string;
+  info: {
+    frameWork: string;
+    technology: string[];
+    linkGit: string;
+    linkWeb: string;
+  };
+}
+
 const Projets = () => {
   const [filterSetings, setFilterSetings] = useState<string[]>([]);
+  const [filteredProjects, setFilteredProjects] = useState<Item[]>([]);
+  const [check, setCheck] = useState(false);
 
   const handelEven = useCallback(
     (nameFilter: string) => {
       const filterDublicate = filterSetings.includes(nameFilter);
+
       if (filterDublicate) {
         setFilterSetings(filterSetings.filter(item => item !== nameFilter));
         return;
@@ -21,10 +37,13 @@ const Projets = () => {
   );
 
   useEffect(() => {
-    ProjectArrya.map(({ info }) => {
-      console.log(info.technology);
-      console.log(filterSetings);
-    });
+    const arrays = ProjectArrya.filter(item =>
+      item.info.technology.some(technology =>
+        filterSetings.includes(technology)
+      )
+    );
+
+    setFilteredProjects(arrays);
   }, [filterSetings]);
 
   return (
@@ -43,13 +62,25 @@ const Projets = () => {
       </PageNameLayout>
 
       <WrapperProject>
-        {ProjectArrya.map(({ id, nameProject, info }) => {
-          return (
-            <Box key={id}>
-              <CardProject id={id} nameProject={nameProject} info={info} />
-            </Box>
-          );
-        })}
+        {filterSetings.length === 0 ? (
+          ProjectArrya.map(({ id, nameProject, info }) => {
+            return (
+              <Box key={id}>
+                <CardProject id={id} nameProject={nameProject} info={info} />
+              </Box>
+            );
+          })
+        ) : filteredProjects.length === 0 ? (
+          <Info>There's no match :( </Info>
+        ) : (
+          filteredProjects.map(({ id, nameProject, info }) => {
+            return (
+              <Box key={id}>
+                <CardProject id={id} nameProject={nameProject} info={info} />
+              </Box>
+            );
+          })
+        )}
       </WrapperProject>
     </Container>
   );
