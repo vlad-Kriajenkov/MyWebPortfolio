84298bd60c115381fbac5da985e3c9348e17677c
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-02 10:36:35 +0300

    Update
    Add media for mobile

--- src/layout/HomeLayout/HomeNavigation/HomeNavigation.styled.tsx
+++ src/layout/HomeLayout/HomeNavigation/HomeNavigation.styled.tsx
@@ -2,24 +2,40 @@ import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
 import { myColor } from 'assets/colors/colors';
 export const Wrapper = styled.div`
+  position: relative;
   display: flex;
 
   border-bottom: 1px solid ${myColor.border.primary};
-  border-top: 1px solid ${myColor.border.primary};
+
   border-top-left-radius: 10px;
   border-top-right-radius: 10px;
+  @media screen and (max-width: 420px) {
+    justify-content: space-between;
+  }
 `;
 
 export const Name = styled.p`
   font-size: 0.9rem;
   font-weight: 400;
-  border-left: 1px solid ${myColor.border.primary};
 
-  border-top-left-radius: 10px;
   width: 21rem;
   padding: 0.75rem 1rem;
   color: #5565e8;
+  @media screen and (max-width: 420px) {
+    padding: 1.06rem 1.13rem;
+  }
 `;
+
+export const WrapperBurger = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    color: #fff;
+    font-size: 2.4rem;
+  }
+`;
+
 export const Nav = styled.nav`
   display: flex;
   width: 100%;
@@ -29,6 +45,26 @@ export const Nav = styled.nav`
   & :last-child {
     margin-left: auto;
     border-top-right-radius: 10px;
+  }
+
+  @media screen and (max-width: 420px) {
+    position: absolute;
+    top: -100dvh;
+    left: 0;
+
+    flex-direction: column;
+    height: 85dvh;
+    background-color: ${myColor.backGraund.main};
+    & :nth-of-type(3) {
+      border-right: none;
+    }
+    & :last-child {
+      margin-left: 0px;
+    }
+    &.active {
+   
+      transform: translateY(107.1dvh);
+    }
   }
 `;
 export const Link = styled(NavLink)`
@@ -43,5 +79,19 @@ export const Link = styled(NavLink)`
   &.active {
     color: #fff;
     border-bottom: 3px solid ${myColor.border.active};
+  }
+  @media screen and (max-width: 420px) {
+    font-size: 1rem;
+
+    width: 100%;
+    text-align: left;
+    padding: 1.06rem 1.13rem;
+
+    color: #fff;
+    border-left: none;
+    border-bottom: 1px solid ${myColor.border.primary};
+    &.active {
+      border-bottom: 1px solid ${myColor.border.primary};
+    }
   }
 `;
--- src/layout/HomeLayout/HomeNavigation/HomeNavigation.tsx
+++ src/layout/HomeLayout/HomeNavigation/HomeNavigation.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
-
-import { Wrapper, Link, Nav, Name } from './HomeNavigation.styled';
+import React, { useState } from 'react';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { IoMdClose } from 'react-icons/io';
+import {
+  Wrapper,
+  Link,
+  WrapperBurger,
+  Nav,
+  Name,
+} from './HomeNavigation.styled';
 const HomeNavigation = () => {
+  const [handelBurger, setHandelBurger] = useState(false);
+
+  const hadelMenu = () => {
+    setHandelBurger(!handelBurger);
+  };
+  const closeBurger = ()=>{
+    setHandelBurger(false)
+  }
   return (
     <Wrapper>
       <Name>krajenkov-vladislav</Name>
-      <Nav>
-        <Link to="/">_home</Link>
-        <Link to="/about">_about</Link>
-        <Link to="/progect">_progect</Link>
-        <Link to="/contact">_contact-me</Link>
+      {handelBurger ? (
+        <WrapperBurger>
+          <IoMdClose onClick={hadelMenu} width="1.2rem" />
+        </WrapperBurger>
+      ) : (
+        <WrapperBurger>
+          <RxHamburgerMenu onClick={hadelMenu} />
+        </WrapperBurger>
+      )}
+
+      <Nav className={handelBurger ? 'active' : ''}>
+        <Link onClick={closeBurger} to="/">_home</Link>
+        <Link onClick={closeBurger} to="/about">_about</Link>
+        <Link onClick={closeBurger} to="/progect">_progect</Link>
+        <Link onClick={closeBurger} to="/contact">_contact-me</Link>
       </Nav>
     </Wrapper>
   );
