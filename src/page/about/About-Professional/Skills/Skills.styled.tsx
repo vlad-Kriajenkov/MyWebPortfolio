3b0cdda4545543900a43082acd42e41dfc3e1f38
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-04 15:54:05 +0300

    Update

--- src/page/about/About-Professional/Skills/Skills.styled.tsx
+++ src/page/about/About-Professional/Skills/Skills.styled.tsx
@@ -35,3 +35,39 @@ export const Wrapper = styled.div`
   width: 933px;
   height: 300px;
 `;
+
+export const FlexBox = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+`;
+
+export const WrapperScils = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin: 10px;
+  padding: 5px;
+
+  background: radial-gradient(
+    circle at bottom right,
+    rgba(127, 73, 99, 0.6) 0,
+    rgba(34, 62, 76, 0.5) 100%
+  );
+  box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.6);
+  backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);
+  border-top: 1px solid rgba(255, 255, 255, 0.25);
+  border-right: 1px solid rgba(152, 152, 152, 0.18);
+  border-left: 1px solid rgba(43, 43, 43, 0.3);
+  border-bottom: 1px solid rgba(43, 43, 43, 0.3);
+  border-radius: 0.75rem;
+  h2 {
+    color: white;
+  }
+  svg {
+    width: 50px;
+    height: 50px;
+    margin-bottom: 10px;
+  }
+`;
--- src/page/about/About-Professional/Skills/Skills.tsx
+++ src/page/about/About-Professional/Skills/Skills.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
-import { Container, SkillAll, Wrapper, Back } from './Skills.styled';
+import {
+  Container,
+  SkillAll,
+  Wrapper,
+  Back,
+  WrapperScils,
+  FlexBox,
+} from './Skills.styled';
 import { IconSkill, PageName } from 'components';
 import skillsData from 'assets/json/skills.json';
 import { nanoid } from 'nanoid';
+import { useMediaQuery } from 'react-responsive';
+
+import Sprite from 'assets/img/skills/sprite.svg';
+
 const Skills = () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 420px)' });
+
+  if (isMobile) {
+    return (
+      <Container>
+        <PageName lable="All" />
+        <FlexBox>
+          {skillsData.map(({ name, nameIcon }) => {
+            return (
+            
+                <WrapperScils key={name}>
+                  <svg>
+                    <use href={Sprite + `${nameIcon}`}></use>
+                  </svg>
+                  <h2>{name}</h2>
+                </WrapperScils>
+              
+            );
+          })}
+        </FlexBox>
+      </Container>
+    );
+  }
+
   return (
     <Container>
       <PageName lable="All" />
@@ -14,7 +49,7 @@ const Skills = () => {
             {skillsData.map(({ name, nameIcon, top, left }) => {
               return (
                 <IconSkill
-                  key={nanoid()} 
+                  key={nanoid()}
                   name={name}
                   idIcon={nameIcon}
                   top={top}
