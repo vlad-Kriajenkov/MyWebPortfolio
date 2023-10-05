52bd9cfe29f5a2174f150899a36ce12e8f48c33c
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-05 18:21:02 +0300

    Delete

--- src/components/Card/Card.tsx
+++
@@ -1,8 +0,0 @@
-import React, { PropsWithChildren } from 'react';
-import {CardWrap} from './Card.styled'
-const Card = (props: PropsWithChildren) => {
-  const { children } = props;
-  return <CardWrap>{children}</CardWrap>;
-};
-
-export { Card };
