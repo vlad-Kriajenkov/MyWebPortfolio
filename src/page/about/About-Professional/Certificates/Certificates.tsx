1347f258b38ab8f767e1ba964d1cdc24c9daf8cf
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-05 18:29:01 +0300

    Update Certificates.tsx

--- src/page/about/About-Professional/Certificates/Certificates.tsx
+++ src/page/about/About-Professional/Certificates/Certificates.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Conatiner, WrapperCard } from './Certificates.styled';
+ 
 import {  CardItem } from 'components';
 import { CardCertificats } from 'components/Cards/CardCertificats';
 const Certificates = () => {
