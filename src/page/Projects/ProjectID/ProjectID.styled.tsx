25601e51f0f5ef9e6aa5c0e166b4db57a3eaa86a
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-05 10:13:04 +0300

    Update ProjectID.styled.tsx

--- src/page/Projects/ProjectID/ProjectID.styled.tsx
+++ src/page/Projects/ProjectID/ProjectID.styled.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
-import { myColor } from 'assets/colors/colors';
-
+ 
+import ImgP from 'assets/img/home/BackgroundBlurs.png';
 export const Container = styled.div`
   flex-grow: 1;
 
@@ -9,9 +9,38 @@ export const Container = styled.div`
 `;
 
 export const WrapperSlider = styled.div`
-  padding: 2rem;
-  border-right: 1px solid ${myColor.border.primary};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-image: url(${ImgP});
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  h2 {
+    font-size: 3.7rem;
+    font-weight: 700;
+    color: white;
+    text-shadow: 2px 2px 2px black;
+  }
 `;
 export const WrapperInfo = styled.div`
   padding: 2rem;
+`;
+
+export const SubTitle = styled.h3`
+  color: white;
+  font-size: 1.9rem;
+`;
+export const TechnologyList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+`;
+export const TechnologyItem = styled.ul`
+  width: max-content;
+
+  background-color: blanchedalmond;
+  border-radius: 6px;
+  margin: 5px;
+  padding: 10px;
 `;
