695caf66221b22e8b7e7c4b174330b6e7a9d8320
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-05 10:13:08 +0300

    Update ProjectID.tsx

--- src/page/Projects/ProjectID/ProjectID.tsx
+++ src/page/Projects/ProjectID/ProjectID.tsx
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { Container, WrapperSlider, WrapperInfo } from './ProjectID.styled';
-import ImgP from 'assets/img/projject/pImg.jpeg';
+import {
+  Container,
+  WrapperSlider,
+  WrapperInfo,
+  SubTitle,
+  TechnologyList,
+  TechnologyItem,
+} from './ProjectID.styled';
+
 import { useParams } from 'react-router-dom';
 import ProjetArray from 'assets/json/project.json';
+import { nanoid } from 'nanoid';
 
 interface Item {
   id: number;
@@ -27,25 +35,27 @@ const ProjectID = () => {
 
   return (
     <Container>
-      <WrapperSlider>
-        <img src={ImgP} alt="" />
-      </WrapperSlider>
       <WrapperInfo>
-        <h2>
-          <span>Name-Project: {project?.nameProject}</span>
-        </h2>
-        <p>
-          <span>About:{project?.about}</span>
-        </p>
         <div>
-          <p>Technology</p>
-          <ul>{project?.info.technology.map(item => item)}</ul>
+          <SubTitle>About</SubTitle>
+          {project?.about}
+        </div>
+        <div>
+          <SubTitle>Technology</SubTitle>
+          <TechnologyList>
+            {project?.info.technology.map(item => {
+              return <TechnologyItem key={nanoid()}>{item}</TechnologyItem>;
+            })}
+          </TechnologyList>
         </div>
         <div>
           <a href={project?.info.linkGit}>linkGit</a>
           <a href={project?.info.linkWeb}>linkWeb</a>
         </div>
       </WrapperInfo>
+      <WrapperSlider>
+        <h2>{project?.nameProject}</h2>
+      </WrapperSlider>
     </Container>
   );
 };
