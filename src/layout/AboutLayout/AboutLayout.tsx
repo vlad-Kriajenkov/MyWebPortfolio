b419e0a3793fbb715a371c7fa5fb5d3cdbd7ebc1
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-06 15:49:11 +0300

    Add handleLinkPageBack

--- src/layout/AboutLayout/AboutLayout.tsx
+++ src/layout/AboutLayout/AboutLayout.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Outlet } from 'react-router';
-
 import { MdFolderShared, MdFolderSpecial } from 'react-icons/md';
-
 import { Conatainer, Nav, Link } from './AboutLayout.styles';
 
- 
 const AboutLayout = () => {
- 
+  const handleLinkPageBack = (flag: string) => {
+    localStorage.setItem('Page', `${flag}`);
+  };
   return (
     <Conatainer>
       <Nav>
-        <Link to="/about/professional">
+        <Link
+          to="/about/professional"
+          onClick={() => handleLinkPageBack('/about/professional')}
+        >
           <MdFolderSpecial />
         </Link>
-        <Link to="/about/personal">
+        <Link
+          to="/about/personal"
+          onClick={() => handleLinkPageBack('/about/personal')}
+        >
           <MdFolderShared />
         </Link>
       </Nav>
