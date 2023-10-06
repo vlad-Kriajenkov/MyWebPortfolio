04974b40c93692080244752039cc0494f4f222ca
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-06 15:37:44 +0300

    update
    add state Navigation Page

--- src/components/Button/ButtonBack/ButtonBack.tsx
+++ src/components/Button/ButtonBack/ButtonBack.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Name, WrapperIcon } from './ButtonBack.styled';
 import { IoClose } from 'react-icons/io5';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate } from 'react-router-dom';
 type Props = {
   contentText: string;
 };
 
 const ButtonBack = (props: Props) => {
   const { contentText } = props;
+  const [navigationPage, setNavigationPage] = useState<string | null>()
   const navigate = useNavigate();
+  
+  useEffect(()=>{
+    let page = localStorage.getItem("Page")
+    setNavigationPage(page)
+  },[])
+
+
+  const goBack = () => navigate(`${navigationPage}`, { replace: true });
   return (
-    <Button onClick={() => navigate(-1)}>
+    <Button onClick={goBack}>
       <Name>{contentText}</Name>
       <WrapperIcon>
         <IoClose />
