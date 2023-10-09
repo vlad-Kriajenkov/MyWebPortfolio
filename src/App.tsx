0a213386786c39770fa8701ea5c652d160107e5f
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-09 10:30:24 +0300

    Add React.lazy
    Fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import {
-  HomeLayout,
-  AboutLayout,
-  AboutProfessionalLayout,
-  AboutPersonalLayout,
-} from './layout';
-import { Frilance, Cpaecom, Skills } from 'page/about';
-import Home from './page/home/Home';
-import { Certificates } from 'page/about/About-Professional/Certificates/Certificates';
-import AboutMe from 'page/about/About-Personal/About-me/AboutMe';
-import Projets from 'page/Projects/Projects';
-import ProjectID from 'page/Projects/ProjectID/ProjectID';
+const HomeLayout = React.lazy(() => import('layout/HomeLayout/HomeLayout'));
+const Home = React.lazy(() => import('./page/home/Home'));
+const AboutLayout = React.lazy(() => import('layout/AboutLayout/AboutLayout'));
+const AboutProfessionalLayout = React.lazy(
+  () => import('layout/AboutProfessionalLayout/AboutProfessionalLayout')
+);
+const Frilance = React.lazy(
+  () => import('page/about/About-Professional/Frilance/Frilance')
+);
+const Cpaecom = React.lazy(
+  () => import('page/about/About-Professional/Cpaecom/Cpaecom')
+);
+const Skills = React.lazy(
+  () => import('page/about/About-Professional/Skills/Skills')
+);
+const Certificates = React.lazy(
+  () => import('page/about/About-Professional/Certificates/Certificates')
+);
+const AboutPersonalLayout = React.lazy(
+  () => import('layout/AboutPersonalLayout/AboutPersonalLayout')
+);
+const AboutMe = React.lazy(
+  () => import('page/about/About-Personal/About-me/AboutMe')
+);
+const Projets = React.lazy(() => import('page/Projects/Projects'));
+const ProjectID = React.lazy(() => import('page/Projects/ProjectID/ProjectID'));
+
 
 function App() {
   return (
