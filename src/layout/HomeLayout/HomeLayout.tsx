3049aa7d65628f01f2de13d670faf191d0aec979
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-11 12:50:43 +0300

    Update
    Add useMediaQuery

--- src/layout/HomeLayout/HomeLayout.tsx
+++ src/layout/HomeLayout/HomeLayout.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import { Conatainer } from './HomeLayout.styled';
 import { HomeNavigation, HomeFooter } from './index';
+import { useMediaQuery } from 'react-responsive';
 const HomeLayout = () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 426px)' });
   return (
     <Conatainer>
       <HomeNavigation />
       <main>
         <Outlet />
       </main>
-      <HomeFooter />
+      {isMobile ? <></> : <HomeFooter />}
     </Conatainer>
   );
-};
+}; 
 
 export default HomeLayout;
