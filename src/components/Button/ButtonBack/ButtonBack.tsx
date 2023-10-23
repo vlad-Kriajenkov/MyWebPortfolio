1b49ff061d52e09142b2d2b578fb787131789581
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-23 12:12:57 +0300

    Change Logic back Button

--- src/components/Button/ButtonBack/ButtonBack.tsx
+++ src/components/Button/ButtonBack/ButtonBack.tsx
@@ -1,21 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Name, WrapperIcon } from './ButtonBack.styled';
 import { IoClose } from 'react-icons/io5';
-import {useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 type Props = {
   contentText: string;
+  navigationPage: string;
 };
 
 const ButtonBack = (props: Props) => {
-  const { contentText } = props;
-  const [navigationPage, setNavigationPage] = useState<string | null>()
-  const navigate = useNavigate();
-  
-  useEffect(()=>{
-    let page = localStorage.getItem("Page")
-    setNavigationPage(page)
-  },[])
+  const { contentText, navigationPage } = props;
 
+  const navigate = useNavigate();
 
   const goBack = () => navigate(`${navigationPage}`, { replace: true });
   return (
